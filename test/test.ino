29e3057c72534326d8e136d8356c6c7ef252711c
Emily Hu <emilyhu1224@gmail.com>
2020-02-03 12:17:01 -0500

    Added mode switch button

--- test/test.ino
+++ test/test.ino
@@ -1,41 +1,75 @@
 /*
+  https://www.arduino.cc/en/tutorial/button
+  https://www.arduino.cc/en/reference/servo
   https://www.arduino.cc/en/Tutorial/Knob
   https://www.pololu.com/product/948
 */
 #include <Servo.h>
 
+const int BUTTON_PIN = 2;
 const int SERVO_PIN = 9;
-const int PERIOD = 500;
-const int MAX_RUNTIME = 20000;
+const int PAUSE_DURATION = 200;
+const int RUN_DURATION = 50;
+
+long triggeredAt = 0;  // timestamp of last mode switch
+long debounce = 200;   // the debounce time
+
+bool isOnRandomMode = false;
 
 Servo servo;
-int currentSpeed = 0;
-long lastUpdated = 0;
-bool stopped = false;
 
 void setup() {
-  Serial.begin(9600);
   servo.attach(SERVO_PIN);
+  pinMode(BUTTON_PIN, INPUT);
+  Serial.begin(9600);
+}
+
+void randomMode() {
+  // Rotate forward at full speed
+  // for RUN_DURATION amount of time
+  servo.write(180);
+  delay(RUN_DURATION);
+  servo.write(90);
+
+  // Wait for PAUSE_DURATION amount of time
+  delay(PAUSE_DURATION);
+  
+  // Rotate forward at full speed
+  // for RUN_DURATION amount of time
+  servo.write(180);
+  delay(RUN_DURATION);
+  servo.write(90);
+
+  // Wait for PAUSE_DURATION amount of time
+  delay(PAUSE_DURATION);
+}
+
+void controlledMode() {
+  // Stop the rotation (shouldn't be necessary)
+  servo.write(90);
+
+  // Wait for PAUSE_DURATION amount of time
+  delay(PAUSE_DURATION * 3);
 }
 
 void loop() {
-  if (stopped) return;
-  
-  if (millis() > MAX_RUNTIME) {
-    servo.write(90);
-    stopped = true;
-    return;
-  }
-  
-  if (millis() - lastUpdated > PERIOD){
-    lastUpdated = millis();
-    Serial.println(currentSpeed);
-    servo.write(currentSpeed);
+  // Read the state of the pushbutton value:
+  if (digitalRead(BUTTON_PIN) == HIGH && (!triggeredAt || millis() - triggeredAt > debounce)) {
+    triggeredAt = millis();
     
-    if (currentSpeed == 180){
-      currentSpeed = 0;
-    } else{
-      currentSpeed = 180;
-    }
+    // Switch mode
+    isOnRandomMode = !isOnRandomMode;
+    Serial.print("Mode switched, isOnRandomMode: ");
+    Serial.println(isOnRandomMode);
+  }
+
+  if (isOnRandomMode) {
+    Serial.println("Starting randomMode.");
+    randomMode();
+    Serial.println("Finished randomMode.");
+  } else {
+    Serial.println("Starting controlledMode.");
+    controlledMode();
+    Serial.println("Finished controlledMode.");
   }
 }
